e21adb70c2ba72dfcb3375e46eee5d7171a5478e
Jesse Akes <akesjesse@gmail.com>
2019-01-26 16:25:33 -0500

    Fixed reset

--- melonJS/examples/isometric_rpg/js/entities/Mom.js
+++ melonJS/examples/isometric_rpg/js/entities/Mom.js
@@ -8,6 +8,7 @@ game.MomEntity = me.Entity.extend({
     init: function(x, y, settings) {
         // call the constructor
         this._super(me.Entity, "init", [x, y , settings]);
+        this.body.collisionType = game.collisionTypes.MOM;
 
         // walking & jumping speed
         this.body.setVelocity(2.5, 2.5);
