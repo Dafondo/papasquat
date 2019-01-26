324232122dff04f502a4d5b744f5716ca3f0ce2c
komalessarani <kessara0@live.unc.edu>
2019-01-26 14:47:48 -0500

    tiles

---
+++ melonJS/examples/isometric_rpg/data/map/block.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="block" tilewidth="64" tileheight="32" tilecount="2" columns="1">
+ <image source="../Users/Komal Essarani/Documents/papasquat/assets/images/block.png" width="64" height="64"/>
+</tileset>
