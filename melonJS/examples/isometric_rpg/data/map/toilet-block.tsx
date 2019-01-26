60345e429bbb189e8f8e98f29e2bbd1c819c7b4e
komalessarani <kessara0@live.unc.edu>
2019-01-26 17:37:05 -0500

    collision addition

---
+++ melonJS/examples/isometric_rpg/data/map/toilet-block.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="toilet-block" tilewidth="64" tileheight="32" tilecount="2" columns="1">
+ <image source="../../../../../assets/images/toilet-block.png" width="64" height="64"/>
+</tileset>
