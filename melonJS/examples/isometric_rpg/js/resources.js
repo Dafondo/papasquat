f04e2d02df176855e1d477f0c888c6f2237d0810
Austin Wang <austin.wang0311@gmail.com>
2019-01-26 10:45:23 -0500

    test tileset

--- melonJS/examples/isometric_rpg/js/resources.js
+++ melonJS/examples/isometric_rpg/js/resources.js
@@ -7,7 +7,8 @@ game.resources = [
     { name: "forest",  type:"image", src: "data/img/forest.png" },
     { name: "meta_collide",  type:"image", src: "data/img/meta_collide.png" },
     { name: "Papa_Squat-01", type:"image", src: "data/img/Papa_Squat-01.png" },
-    { name: "floortiles", type:"image", src: "data/img/floortiles.png"},
+    { name: "floorimages", type:"image", src: "data/img/floorimages.png"},
+    { name: "floortiles", type:"tsx", src: "data/map/floortiles.tsx"},
 
     /* Maps.
      * @example
