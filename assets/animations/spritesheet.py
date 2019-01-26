99609addbbf9770ef2f30ebc4c9279018b90f9a2
Hastings Greer <hastings@hgreer.com>
2019-01-26 12:28:19 -0500

    transparent animateions

--- assets/animations/spritesheet.py
+++ assets/animations/spritesheet.py
@@ -13,12 +13,14 @@ print(files)
 images = []
 for f in files:
    print(f)
-   images.append(cv2.imread(folder + "/" + f))
+   images.append(cv2.imread(folder + "/" + f, cv2.IMREAD_UNCHANGED))
    
    
    cv2.imshow("k", images[-1])
+   print(images[-1].shape)
    cv2.waitKey(30)
 x = np.concatenate(images, axis=1)
 cv2.imshow("k", x)
-cv2.waitKey(343453530)
+cv2.waitKey(343)
 cv2.imwrite(folder[:-2] + "sheet.png", x)
+print(x.shape)
--- melonJS/examples/isometric_rpg/js/resources.js
+++ melonJS/examples/isometric_rpg/js/resources.js
@@ -7,8 +7,9 @@ game.resources = [
     { name: "forest",  type:"image", src: "data/img/forest.png" },
     { name: "meta_collide",  type:"image", src: "data/img/meta_collide.png" },
     { name: "Papa_Squat-01", type:"image", src: "data/img/Papa_Squat-01.png" },
+    { name: "Papa_Pee", type:"image", src: "data/img/peetimsheet.png" },
     { name: "Mama_Squat-01", type:"image", src: "data/img/Mama_Squat-01.png" },
-    { name: "plant", type:"image", src: "data/img/PLANT-healthly-01.png"},
+    { name: "plant", type:"image", src: "data/img/plansheet.png"},
 
     { name: "floorimages", type:"image", src: "data/img/floorimages.png"},
     { name: "floortiles", type:"tsx", src: "data/map/floortiles.tsx"},
