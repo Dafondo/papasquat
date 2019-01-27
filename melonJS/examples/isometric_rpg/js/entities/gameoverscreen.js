f0cd7e9eacba59d6f0efd38eaf26e0629ced8813
Nikhil Vytla <nvawesomeguy@gmail.com>
2019-01-27 13:40:05 -0500

    added play text to gameover and gamestart

--- melonJS/examples/isometric_rpg/js/entities/gameoverscreen.js
+++ melonJS/examples/isometric_rpg/js/entities/gameoverscreen.js
@@ -109,3 +109,74 @@ game.EndScreen.NewsItem = me.Renderable.extend( {
   }
 });
 
+// Lorem ipsum dolor sit amet,
+// consectetur adipiscing elit,
+// sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+// Libero nunc consequat interdum varius sit amet mattis vulputate enim.
+// Luctus accumsan tortor posuere ac. Faucibus nisl tincidunt eget nullam.
+// Quam lacus suspendisse faucibus interdum posuere lorem.
+// Natoque penatibus et magnis dis.
+// Eget aliquet nibh praesent tristique magna.
+// Nec nam aliquam sem et tortor consequat id.
+// Amet cursus sit amet dictum sit.
+// Ultrices tincidunt arcu non sodales.
+// A iaculis at erat pellentesque adipiscing commodo elit at imperdiet.
+
+// Ornare massa eget egestas purus viverra.
+// Netus et malesuada fames ac turpis egestas integer eget aliquet.
+// At volutpat diam ut venenatis.
+// Ornare lectus sit amet est placerat in egestas erat.
+// Sed lectus vestibulum mattis ullamcorper.
+// Velit egestas dui id ornare arcu odio ut.
+// Turpis egestas maecenas pharetra convallis posuere morbi.
+// Vitae tempus quam pellentesque nec nam.
+// Feugiat nisl pretium fusce id velit ut tortor.
+// Ac turpis egestas sed tempus.
+
+// Pharetra vel turpis nunc eget.
+// Rhoncus dolor purus non enim praesent elementum facilisis.
+// Nec ultrices dui sapien eget mi proin.
+// Laoreet non curabitur gravida arcu ac tortor.
+// Pellentesque pulvinar pellentesque habitant morbi. 
+// aculis eu non diam phasellus vestibulum lorem.
+// Viverra tellus in hac habitasse platea dictumst.
+// Iaculis urna id volutpat lacus laoreet non curabitur gravida arcu.
+// Ullamcorper velit sed ullamcorper morbi tincidunt ornare.
+// Porttitor massa id neque aliquam vestibulum morbi blandit cursus.
+// Fringilla est ullamcorper eget nulla facilisi.
+// Ultricies mi eget mauris pharetra.
+// Odio aenean sed adipiscing diam donec adipiscing tristique risus nec.
+// Sed arcu non odio euismod lacinia.
+// Odio pellentesque diam volutpat commodo sed egestas egestas.
+
+// Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu.
+// Massa enim nec dui nunc mattis enim ut.
+// Aliquam sem fringilla ut morbi tincidunt augue interdum velit.
+// Eu feugiat pretium nibh ipsum consequat nisl vel.
+// Justo laoreet sit amet cursus sit.
+// In ornare quam viverra orci sagittis eu.
+// Sit amet porttitor eget dolor morbi non.
+// Adipiscing vitae proin sagittis nisl rhoncus.
+// Nam libero justo laoreet sit amet cursus sit amet dictum.
+// Justo eget magna fermentum iaculis eu non diam.
+
+// Ipsum dolor sit amet consectetur adipiscing elit.
+// Id consectetur purus ut faucibus pulvinar elementum.
+// Cras fermentum odio eu feugiat.
+// Nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+// Nec ultrices dui sapien eget mi proin.
+// Odio euismod lacinia at quis risus sed vulputate odio ut.
+// Urna cursus eget nunc scelerisque viverra mauris in.
+// Pretium viverra suspendisse potenti nullam ac.
+// Semper risus in hendrerit gravida.
+// Ultrices mi tempus imperdiet nulla malesuada.
+// Diam maecenas sed enim ut sem viverra.
+// Massa tincidunt nunc pulvinar sapien.
+// Id semper risus in hendrerit gravida rutrum.
+// Ac turpis egestas maecenas pharetra.
+// Ut ornare lectus sit amet est placerat.
+// Vitae tortor condimentum lacinia quis vel eros.
+// Varius duis at consectetur lorem.
+// Orci nulla pellentesque dignissim enim sit amet venenatis urna.
+// Leo vel fringilla est ullamcorper.
+// Purus ut faucibus pulvinar elementum integer enim neque volutpat ac.
