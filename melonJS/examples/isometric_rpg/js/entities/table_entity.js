784fb91eb0c059bad8a429a961aeb2ba69acc3cb
Austin Wang <austin.wang0311@gmail.com>
2019-01-27 10:58:09 -0500

    CRUNCH SOUNDS PLAY FOLLOWING FOOD ACQUISITION

--- melonJS/examples/isometric_rpg/js/entities/table_entity.js
+++ melonJS/examples/isometric_rpg/js/entities/table_entity.js
@@ -11,7 +11,7 @@ game.TableEntity = me.Entity.extend({
         this._super(me.Entity, "init", [x, y , {width:48, height:32}]);
         this.body.collisionType = game.collisionTypes.PLANT;
 
-        
+
         var texture =  new me.video.renderer.Texture(
             { framewidth: 64, frameheight: 128 },
             me.loader.getImage("table")
@@ -20,20 +20,20 @@ game.TableEntity = me.Entity.extend({
         // create a new sprite object
         this.renderable = texture.createAnimationFromName([0, 1, 2, 3, 4]);
         // define an additional basic walking animation
-        
+
         this.renderable.addAnimation ("a", [0]);
         this.renderable.addAnimation ("b", [1]);
         this.renderable.addAnimation ("empty", [2]);
         this.renderable.addAnimation ("c", [3]);
         this.renderable.addAnimation ("d", [4]);
-        
+
         this.renderable.setCurrentAnimation(["a", "b", "c", "d"][Math.floor(Math.random() * 4)]);
-        
+
         this.state = "full";
 
         // set the renderable position to bottom center
         this.anchorPoint.set(0.5, -.7);
-        
+
         me.input.registerPointerEvent("pointerdown", this, this.onMouseDown.bind(this));
     },
 
@@ -43,12 +43,12 @@ game.TableEntity = me.Entity.extend({
 
     ------            */
     update : function (dt) {
-        
-        
+
+
     },
-    
+
     onMouseDown : function() {
-        if(Math.pow(Math.pow(this.pos.x - game.data.player.pos.x, 2) + 
+        if(Math.pow(Math.pow(this.pos.x - game.data.player.pos.x, 2) +
            Math.pow(this.pos.y - game.data.player.pos.y, 2), .5) > 120){
            game.data.messages.push("too far to interact");
            return false;
@@ -61,19 +61,19 @@ game.TableEntity = me.Entity.extend({
             this.renderable.setCurrentAnimation("empty");
             // make it flicker
             //this.renderable.flicker(75);
-            
-            me.audio.play("Food Get", false, null, .5);
-            
+
+            me.audio.play("Food Get", false, this.eatFood(), .5);
+
 
             game.data.food = 100;
 
-           
+
             return false;
 
         }
         game.data.messages.push("the plate was empty :(");
-        
-},
+
+    },
 
     /**
      * colision handler
@@ -82,6 +82,13 @@ game.TableEntity = me.Entity.extend({
     onCollision : function (/*response, other*/) {
         // Make all other objects solid
         return false;
+    },
+
+    eatFood : function() {
+        me.audio.play("Crunch 2", true, null, 0.7);
+        setTimeout(function() {
+            me.audio.stop("Crunch 2")
+        }, 3500);
     }
 });
 
