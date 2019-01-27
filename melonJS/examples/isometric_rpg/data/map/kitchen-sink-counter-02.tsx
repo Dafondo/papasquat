3c068ff4aa72d98a51b9858d0f0d9c62302a0a73
komalessarani <kessara0@live.unc.edu>
2019-01-26 19:46:01 -0500

    added fridge

---
+++ melonJS/examples/isometric_rpg/data/map/kitchen-sink-counter-02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="kitchen-sink-counter-02" tilewidth="64" tileheight="32" tilecount="3" columns="1">
+ <image source="../../../../../assets/images/kitchen-sink-counter-02.png" width="96" height="96"/>
+</tileset>
