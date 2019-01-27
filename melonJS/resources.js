89f1e4cd3445ebd599997c8e1ef3e34bd1c2dbcf
komalessarani <kessara0@live.unc.edu>
2019-01-26 21:02:55 -0500

    add floorplan

---
+++ melonJS/resources.js
@@ -0,0 +1,87 @@
+game.resources = [
+
+    /* Graphics.
+     * @example
+     * { name: "example", type:"image", src: "data/img/example.png" },
+     */
+    { name: "forest",  type:"image", src: "data/img/forest.png" },
+    { name: "meta_collide",  type:"image", src: "data/img/meta_collide.png" },
+
+    { name: "Papa_Squat-01", type:"image", src: "data/img/walksisheet.png" },
+
+    
+
+    { name: "Papa_Pee", type:"image", src: "data/img/peetisheet.png" },
+    { name: "Mama_Squat-01", type:"image", src: "data/img/mamawalsheet.png" },
+    { name: "Son_Squat-01", type:"image", src: "data/img/sonwalsheet.png" },
+    { name: "Susie_Squat-01", type:"image", src: "data/img/susiewalsheet.png" },
+    { name: "plant", type:"image", src: "data/img/plasheet.png"},
+
+    { name: "floortiles", type:"image", src: "data/img/floortiles.png"},
+    { name: "floortiles", type:"tsx", src: "data/map/floortiles.tsx"},
+
+    { name: "block", type:"image", src: "data/img/block.png"},
+    { name: "block", type:"tsx", src: "data/map/block.tsx"},
+
+    { name: "sofaimages", type:"image", src: "data/img/sofaimages.png"},
+    { name: "sofaimages", type:"tsx", src: "data/map/sofaimages.tsx"},
+
+    { name: "toilet-block", type:"image", src: "data/img/toilet-block.png"},
+    { name: "toilet-block", type:"tsx", src: "data/map/toilet-block.tsx"},
+
+    { name: "toilet-block2", type:"image", src: "data/img/toilet-block2.png"},
+    { name: "toilet-block2", type:"tsx", src: "data/map/toilet-block.tsx"},
+
+    { name: "tub_shower-02", type:"image", src: "data/img/tub_shower-02.png"},
+    { name: "tub_shower-02", type:"tsx", src: "data/map/tub_shower-02.tsx"},
+
+    { name: "stove1-02", type:"image", src: "data/img/stove1-02.png"},
+    { name: "stove1-02", type:"tsx", src: "data/map/stove1-02.tsx"},
+
+    { name: "bathroom-sink-01", type:"image", src: "data/img/bathroom-sink-01.png"},
+    { name: "bathroom-sink-01", type:"tsx", src: "data/map/bathroom-sink-01.tsx"},
+
+    { name: "wardrobe-block", type:"image", src: "data/img/wardrobe-block.png"},
+    { name: "wardrobe-block", type:"tsx", src: "data/map/wardrobe-block.tsx"},
+
+    { name: "BED-green-01", type:"image", src: "data/img/BED-green-01.png"},
+    { name: "BED-green-01", type:"tsx", src: "data/map/BED-green-01.tsx"},
+
+    { name: "BED-parents-01", type:"image", src: "data/img/BED-parents-01.png"},
+    { name: "BED-parents-01", type:"tsx", src: "data/map/BED-parents-01.tsx"},
+
+    { name: "BED-pink-01", type:"image", src: "data/img/BED-pink-01.png"},
+    { name: "BED-pink-01", type:"tsx", src: "data/map/BED-pink-01.tsx"},
+    
+    { name: "END-SCREEN", type:"image", src: "data/img/END-SCREEN-01.png"},
+
+    /* Maps.
+     * @example
+     * { name: "example01", type: "tmx", src: "data/map/example01.tmx" },
+     * { name: "example01", type: "tmx", src: "data/map/example01.json" },
+      */
+    { name: "isometric", type: "tmx", src: "data/map/floorplan2.tmx" },
+
+    /* Background music.
+     * @example
+     * { name: "example_bgm", type: "audio", src: "data/bgm/" },
+     */
+
+    /* Sound effects.
+     * @example
+     * { name: "example_sfx", type: "audio", src: "data/sfx/" }
+     */
+    { name: "Piss", type: "audio", src: "data/sfx/" },
+    { name: "Lose", type: "audio", src: "data/sfx/" },
+    { name: "Gameplay Theme (Day)", type: "audio", src: "data/sfx/" },
+    { name: "Game Over Screen Theme", type: "audio", src: "data/sfx/" },
+    { name: "Toilet Flush", type: "audio", src: "data/sfx/" },
+    { name: "Cabinet Close", type: "audio", src: "data/sfx/" },
+    { name: "Cabinet Open", type: "audio", src: "data/sfx/" },
+    { name: "BREAKING NEWS", type: "audio", src: "data/sfx/" },
+    /* Atlases
+     * @example
+     * { name: "example_tps", type: "json", src: "data/img/example_tps.json" },
+     */
+
+];
