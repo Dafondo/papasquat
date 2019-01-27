ec253968d71628119e1c386274a0de9f04b03413
komalessarani <kessara0@live.unc.edu>
2019-01-26 20:50:26 -0500

    kitchen 2

--- melonJS/examples/isometric_rpg/data/map/kitchen-sink-counter-02.tsx
+++ melonJS/examples/isometric_rpg/data/map/kitchen-sink-counter-02.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="kitchen-sink-counter-02" tilewidth="64" tileheight="32" tilecount="3" columns="1">
- <image source="../../../../../assets/images/kitchen-sink-counter-02.png" width="96" height="96"/>
+<tileset version="1.2" tiledversion="1.2.1" name="kitchen-sink-counter-02" tilewidth="64" tileheight="32" tilecount="8" columns="2">
+ <image source="../../../../../assets/images/kitchen-sink-counter-02.png" width="128" height="128"/>
 </tileset>
