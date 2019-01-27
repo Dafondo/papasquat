f958a192b7387de3ca79efea83a37b5a7dd3520e
Austin Wang <austin.wang0311@gmail.com>
2019-01-27 13:57:58 -0500

    RESET DAY/NIGHT TIMER

--- melonJS/examples/isometric_rpg/js/screens/play.js
+++ melonJS/examples/isometric_rpg/js/screens/play.js
@@ -51,7 +51,7 @@ game.PlayScreen = me.Stage.extend({
             }
         }
 
-        game.data.tid = me.timer.setInterval(() => {this.nighttime(this.HUD)} , 10000);
+        game.data.tid = me.timer.setInterval(() => {this.nighttime(this.HUD)} , 30000);
 
         me.audio.stop("Start Theme");
         me.audio.play("Gameplay Theme (Day)", true, null, 0.3);
